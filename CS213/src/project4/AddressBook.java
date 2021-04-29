cb6e1d5bb41a62a68e736e65207ee7cf9e74bbd2
peter <peter@pool-108-21-203-244.nycmny.fios.verizon.net>
2021-04-28 23:22:26 -0400

    Working Project Binary trees

---
+++ CS213/src/project4/AddressBook.java
@@ -0,0 +1,46 @@
+package project4;
+
+public class AddressBook {
+	private int count;
+	private BinarySearchTree<Contact> tree;
+	private TreeIterator<Contact> itr;
+	
+	public AddressBook() { //Instantiate a tree for this new address book, and give it an iterator
+		tree = new BinarySearchTree<Contact>();
+		itr = this.tree.iterator();
+	}
+	
+	public void addContact(Contact c)
+	{
+		tree.insert(c);
+		count++;
+	}
+	
+	public void removeContact(Contact c)
+	{
+		tree.delete(c);
+		count--;
+	}
+	
+	public Contact search(Contact c) {
+		return tree.search(tree.root, c).getElement();
+	}
+	
+	public void removeAll()
+	{
+		tree.root = null;
+		count = 0;
+	}
+	public boolean isEmpty()
+	{
+		return tree.isEmpty();
+	}
+	
+	public BinarySearchTree<Contact> getTree() {
+		return this.tree;
+	}
+	
+	public TreeIterator<Contact> getIterator(){
+		return this.itr;
+	}
+}
